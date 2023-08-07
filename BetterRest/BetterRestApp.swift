eee4912f0eafc6bc0897f6121ae9be75ff875ab3
natashasosa <141663658+natashasosa@users.noreply.github.com>
2023-08-07 14:45:35 -0300

    Initial commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Natasha Rebelo on 02/08/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
